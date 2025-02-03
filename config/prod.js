8c7447a2975ec8d3c7e8001e4513a455d04c81e9
chen <levavi.chen@gmail.com>
2025-02-04 01:02:40 +0200

    working backend

--- config/prod.js
+++ config/prod.js
@@ -1,10 +1,11 @@
 
+// export default {
+//     dbURL: 'mongodb+srv://theUser:thePass@cluster0-klgzh.mongodb.net/test?retryWrites=true&w=majority',
+//     dbName: process.env.DB_NAME || 'board_db'
+// }
+
 export default {
-    dbURL: 'mongodb+srv://theUser:thePass@cluster0-klgzh.mongodb.net/test?retryWrites=true&w=majority',
-    dbName: process.env.DB_NAME || 'board_db'
+  dbURL: process.env.MONGO_URL || 'mongodb+srv://levavichen:chen@donotello.4yvmn.mongodb.net/?retryWrites=true&w=majority&appName=DoNotello',
+  dbName : process.env.DB_NAME || 'DoNotello_db'
 }
 
-// export default {
-//   dbURL: process.env.MONGO_URL || 'mongodb+srv://theUser:thePass@cluster0-klgzh.mongodb.net/test?retryWrites=true&w=majority',
-//   dbName : process.env.DB_NAME || 'tester_db'
-// }
