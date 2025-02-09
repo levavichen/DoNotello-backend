a142166ca16bef3cebc00b182cad9a9deb98f852
chen <levavi.chen@gmail.com>
2025-02-09 23:10:16 +0200

    final checking

--- config/index.js
+++ config/index.js
@@ -2,21 +2,23 @@ import configProd from './prod.js'
 import configDev from './dev.js'
 
 
-export var config = {
-    isGuestMode: true,
-    ...configProd
-}
-
-// if (process.env.NODE_ENV === 'production') {
-//     config = configProd
-// } else {
-//     config = configDev
+// export var config = {
+//     isGuestMode: true,
+//     ...configProd
 // }
+
+export var config
+
+if (process.env.NODE_ENV === 'production') {
+    config = configProd
+} else {
+    config = configDev
+}
 
 
 
 // * Uncomment the following line to use guest mode
-config.isGuestMode = true
+// config.isGuestMode = true
 
 
 //* Uncomment the following line to use the production configuration (Mongo Atlas DB)
